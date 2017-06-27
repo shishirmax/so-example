de91f82a4ceee31a54a9e7612defcce1fc6d8e26
Shishir <shishirmax@gmail.com>
2017-06-27 16:34:51 +0530

    prototype property

--- commonProgram.js
+++ commonProgram.js
@@ -59,3 +59,27 @@ polymorphism: depending on situation diff behaviour
 */
 
 //prototype:to share properties among objects
+function PrintReport(ReportDocument){
+  this.reportDoc = ReportDocument;
+}
+// We add the print () method to PrintStuff prototype
+//property so that other instances (objects) can inherit it:
+PrintReport.prototype.print = function(){
+  console.log(this.reportDoc);
+}
+
+/*
+// Create a new object with the PrintStuff () constructor,
+thus allowing this new object to inherit PrintStuff's properties and methods
+*/
+
+var newObject = new PrintReport("Hey printing the first report.");
+
+/*
+​// newObj inherited all the properties and methods, including the print method,
+from the PrintStuff function. Now newObj can call print directly,
+even though we never created a print () method on it.​
+newObj.print (); //I am a new Object and I can print.
+
+*/
+newObject.print();
